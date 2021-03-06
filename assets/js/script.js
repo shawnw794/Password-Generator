fa836617a5353b53315302a68bd331b57432f521
Shawn Williams <shawnw794@gmail.com>
2021-03-06 12:15:37 -0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+const characterAmountRange
+const characterAmountNumber
+const includeUppercaseElement
+const addEventListener remove this
+
+
+form.addEventListener('')
